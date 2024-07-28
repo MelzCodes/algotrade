7b1bdfaacd07da20516553bd3b190fb1c24eb4f2
MelzCodes <matteo.melis2012@gmail.com>
2024-07-28 17:28:45 +0100

    init

---
+++ src/main/java/com/algotrade/AlgotradeApplication.java
@@ -0,0 +1,13 @@
+package com.algotrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgotradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgotradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algotrade/AlgotradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algotrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgotradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
