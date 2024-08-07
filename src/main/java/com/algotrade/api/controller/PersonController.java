dd9f55837ad977e7ff4b9d0e18f60ec4cad8ef7c
MelzCodes <matteo.melis2012@gmail.com>
2024-08-07 20:29:55 +0100

    Creating base user table template

--- src/main/java/com/algotrade/api/controller/PersonController.java
+++ src/main/java/com/algotrade/api/controller/PersonController.java
@@ -5,7 +5,6 @@ import com.algotrade.api.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
